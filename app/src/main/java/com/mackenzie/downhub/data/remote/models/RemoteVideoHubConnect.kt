a07291e1952a2d8abbdc4042252297e61b804c5d
mackenzie <principemackenzie@gmail.com>
2026-04-01 22:45:29 +0200

    added responses

---
+++ app/src/main/java/com/mackenzie/downhub/data/remote/models/RemoteVideoHubConnect.kt
@@ -0,0 +1,7 @@
+package com.mackenzie.downhub.data.remote.models
+
+import com.mackenzie.downhub.data.remote.service.VideoHubService
+
+data class RemoteVideoHubConnect(
+    val videoHubService: VideoHubService
+)
