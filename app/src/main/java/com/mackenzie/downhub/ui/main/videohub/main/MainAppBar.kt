6b35504e314ddb9faaf69c3b8c0a969a5bbb616f
mackenzie <principemackenzie@gmail.com>
2026-04-02 12:31:06 +0200

    fix imports

--- app/src/main/java/com/mackenzie/downhub/ui/main/videohub/main/MainAppBar.kt
+++ app/src/main/java/com/mackenzie/downhub/ui/main/videohub/main/MainAppBar.kt
@@ -24,8 +24,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mackenzie.downhub.R
-import com.mackenzie.downhub.util.hub.getExternalPlayerMode
-import com.mackenzie.downhub.util.hub.setExternalPlayerMode
+import com.mackenzie.downhub.ui.main.videohub.common.getExternalPlayerMode
+import com.mackenzie.downhub.ui.main.videohub.common.setExternalPlayerMode
 
 @Preview(showBackground = true)
 @OptIn(ExperimentalMaterial3Api::class)
