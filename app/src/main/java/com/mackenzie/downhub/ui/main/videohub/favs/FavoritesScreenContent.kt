75408bdd0d99281b068f13c122b3ac3e6ba85273
mackenzie <principemackenzie@gmail.com>
2026-04-05 21:20:16 +0200

    added favotireScreen

---
+++ app/src/main/java/com/mackenzie/downhub/ui/main/videohub/favs/FavoritesScreenContent.kt
@@ -0,0 +1,43 @@
+package com.mackenzie.downhub.ui.main.videohub.favs
+
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.mackenzie.downhub.ui.main.videohub.main.MainAppBar
+import com.mackenzie.downhub.ui.main.videohub.main.ServerList
+
+class FavoritesScreenContent {
+}
+
+@Preview
+@Composable
+fun FavoritesScreenContent(
+    onSettingsButtonClicked: (() -> Unit) = {},
+    onNavigate: (Int, String) -> Unit = { serverId, serverUrl -> }
+) {
+
+    Scaffold(
+        topBar = { MainAppBar(onSettingsButtonClicked) }
+    ) { padding ->
+
+        val ctx = LocalContext.current
+
+        ServerList(
+            itemSection01 = emptyList(), // No video servers in favorites
+            itemSection02 = emptyList(),
+            itemSection03 = emptyList(), // No hentai servers in favorites
+            padding= padding,
+            onFavoriteClick = {
+                Toast.makeText( ctx, "Favorite Feature Under Development!", Toast.LENGTH_SHORT).show()
+            }
+        ) { item ->
+            Log.e( "VideoHubScreenContent", "Server ID=${item.id}, Clicked Server: ${item.title}")
+            onNavigate(item.id, item.url)
+        }
+
+        Toast.makeText( ctx, "Under Development!", Toast.LENGTH_SHORT).show()
+    }
+}
